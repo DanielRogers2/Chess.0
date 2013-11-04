a6695df7e764be6ae585a4a9b9c6ef0bdf3e5ddf
Daniel Rogers <djamarr@gmail.com>
2013-11-04 17:03:38 -0500

    Added main stub

---
+++ src/main.c
@@ -0,0 +1,25 @@
+/*
+ * main.c
+ * 
+ * Executes the program
+ * 
+ * @author Daniel Rogers
+ * 
+ */
+
+#include "board.h"
+#include "pregame.h"
+
+int main()
+{
+    //Load or generate tables
+
+
+    //Set up network code
+
+
+    //Run the main program loop
+
+
+    return (0);
+}
