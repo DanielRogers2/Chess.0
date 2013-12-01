381674a92c95c43c5f06371959f18b6ff5fb674b
Daniel Rogers <djamarr@gmail.com>
2013-11-30 22:22:36 -0500

    Pull example game debug code into own debug function 	- Also output piece characters & move notation for easier replay of 	  debug games

--- src/main.c
+++ src/main.c
@@ -12,6 +12,10 @@
 #include "pregame.h"
 #include "brain.h"
 
+#ifdef DEBUG
+void playSampleGame(unsigned gamenum, uint8_t w_ply, uint8_t b_ply);
+#endif
+
 int main()
 {
     //Load or generate tables
@@ -65,145 +69,16 @@ int main()
 
     //Game examples
     puts("testing game 4(w) vs 2(b)");
-    uint16_t counter = 0;
-    while (current_state.w_pieces[15] != CAPTURED
-            && current_state.b_pieces[15] != CAPTURED)
-    {
-        //white, to depth 4
-        printf("white: turn %d\n", counter);
-        selectBestMove(true, &current_state, &res, 4);
-        printf("piece: %d, move: %d, value: %d\n", res.last_piece,
-                res.last_move, evaluateState(&res, true));
-        ++counter;
-        current_state = res;
+    playSampleGame(1, 4, 2);
 
-        //black, to depth 2
-        printf("black: turn %d\n", counter);
-        selectBestMove(false, &current_state, &res, 2);
-        printf("piece: %d, move: %d, value: %d\n", res.last_piece,
-                res.last_move, evaluateState(&res, false));
-        current_state = res;
-        ++counter;
-
-        if (counter == 1000)
-        {
-            puts("stalemate maybe...");
-            break;
-        }
-    }
-
-    puts("board state 1");
-    printBoard(&current_state);
-
-    if (current_state.w_locations[15])
-    {
-        puts("winner was 4-ply white! yay it.. works?");
-    }
-    else if (current_state.b_locations[15])
-    {
-        puts("2-ply black won.. back to drawing board");
-    }
-    else
-    {
-        puts("uh-oh");
-    }
-
-    initBoard(&current_state);
     puts("testing game 2(w) vs 4(b)");
-    counter = 0;
-    while (current_state.w_pieces[15] != CAPTURED
-            && current_state.b_pieces[15] != CAPTURED)
-    {
-        //white, to depth 2
-        selectBestMove(true, &current_state, &res, 2);
-        printf("piece: %d, move: %d, value: %d\n", res.last_piece,
-                res.last_move, evaluateState(&res, true));
-        ++counter;
-        current_state = res;
+    playSampleGame(1, 2, 4);
 
-        //black, to depth 4
-        selectBestMove(false, &current_state, &res, 4);
-        printf("piece: %d, move: %d, value: %d\n", res.last_piece,
-                res.last_move, evaluateState(&res, false));
-        ++counter;
-        current_state = res;
-
-        printf("turn %d\n", counter);
-        if (counter == 1000)
-        {
-            puts("stalemate maybe...");
-            break;
-        }
-    }
-
-    puts("board state 2");
-    printBoard(&current_state);
-
-    if (current_state.w_locations[15])
-    {
-        puts("2-ply white won.. back to drawing board");
-    }
-    else if (current_state.b_locations[15])
-    {
-        puts("winner was 4-ply black! yay it.. works?");
-    }
-    else
-    {
-        puts("uh-oh");
-    }
-
-    //Timers
-    clock_t tstart, tend;
-    double tex;
-
-    initBoard(&current_state);
     puts("testing game 4(w) vs 5(b)");
-    counter = 0;
-    while (current_state.w_pieces[15] != CAPTURED
-            && current_state.b_pieces[15] != CAPTURED)
-    {
-        //white, to depth 4
-        printf("white: turn %d\n", counter);
-        selectBestMove(true, &current_state, &res, 4);
-        printf("piece: %d, move: %d, value: %d\n", res.last_piece,
-                res.last_move, evaluateState(&res, true));
-        ++counter;
-        current_state = res;
+    playSampleGame(1, 4, 5);
 
-        //black, to depth 5
-        printf("black: turn %d\n", counter);
-        tstart = clock();
-        selectBestMove(false, &current_state, &res, 5);
-        tend = clock();
-
-        tex = (double) (tend - tstart) / CLOCKS_PER_SEC;
-        printf("piece: %d, move: %d, value: %d in %f\n", res.last_piece,
-                res.last_move, evaluateState(&res, false), tex);
-        ++counter;
-        current_state = res;
-
-        if (counter == 1000)
-        {
-            puts("stalemate maybe...");
-            break;
-        }
-    }
-
-    puts("board state 3");
-    printBoard(&current_state);
-
-    if (current_state.w_locations[15])
-    {
-        puts("4-ply white won.. back to drawing board");
-    }
-    else if (current_state.b_locations[15])
-    {
-        puts("5-ply black won! yay it.. works?");
-    }
-    else
-    {
-        puts("uh-oh");
-    }
+    puts("testing game 5(w) vs 5(b)");
+    playSampleGame(1, 5, 5);
 #endif
 
     //Set up network code
@@ -212,3 +87,63 @@ int main()
 
     return (0);
 }
+
+#ifdef DEBUG
+void playSampleGame(unsigned gamenum, uint8_t w_ply, uint8_t b_ply)
+{
+    //Timers
+    clock_t tstart, tend;
+    double tex;
+
+    chessboard current_state;
+    chessboard res;
+
+    char mov_str[3];
+
+    initBoard(&current_state);
+    puts("testing game 4(w) vs 5(b)");
+    uint8_t counter = 0;
+    while (current_state.w_pieces[15] != CAPTURED
+            && current_state.b_pieces[15] != CAPTURED)
+    {
+        //white
+        printf("white: turn %d\n", counter);
+        tstart = clock();
+        selectBestMove(true, &current_state, &res, w_ply);
+        tend = clock();
+
+        squareToString(res.last_move, mov_str);
+        tex = (double) (tend - tstart) / CLOCKS_PER_SEC;
+
+        printf("piece: %c, move: %s, value: %d in %f\n",
+                piece_chars[res.last_piece], mov_str, evaluateState(&res, true),
+                tex);
+        ++counter;
+        current_state = res;
+
+        //black
+        printf("black: turn %d\n", counter);
+        tstart = clock();
+        selectBestMove(false, &current_state, &res, b_ply);
+        tend = clock();
+
+        squareToString(res.last_move, mov_str);
+        tex = (double) (tend - tstart) / CLOCKS_PER_SEC;
+
+        printf("piece: %c, move: %s, value: %d in %f\n",
+                piece_chars[res.last_piece], mov_str,
+                evaluateState(&res, false), tex);
+        ++counter;
+        current_state = res;
+
+        if (counter == 0)
+        {
+            puts("stalemate maybe...");
+            break;
+        }
+    }
+
+    printf("board state %u", gamenum);
+    printBoard(&current_state);
+}
+#endif
